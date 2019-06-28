68c00bc0a7896b8bd56d4a32830ddbf2a25b2c1d
milan <milanps92@gmail.com>
2019-06-28 20:22:37 +0200

    Initial commit, libGDX

---
+++ core/src/com/milanps/coinman/game/CoinMan.java
@@ -0,0 +1,55 @@
+package com.milanps.coinman.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class CoinMan extends ApplicationAdapter {
+    SpriteBatch batch;
+    Texture background;
+    Texture[] man;
+    int manState;
+    int pause;
+
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        background = new Texture("bg.png");
+        man = new Texture[4];
+        man[0] = new Texture("frame-1.png");
+        man[1] = new Texture("frame-2.png");
+        man[2] = new Texture("frame-3.png");
+        man[3] = new Texture("frame-4.png");
+
+    }
+
+    @Override
+    public void render() {
+        batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        if (pause < 8) {
+            pause++;
+        } else {
+            pause=0;
+            if (manState < 3) {
+                manState++;
+            } else {
+                manState = 0;
+            }
+        }
+        batch.draw(man[manState], Gdx.graphics.getWidth() / 2 - man[manState].getWidth() / 1, Gdx.graphics.getHeight() / 4);
+
+        batch.end();
+
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+
+    }
+}
